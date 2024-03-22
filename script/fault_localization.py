97e9d90bf1db6fedad078155731b26d21380b773
Zehao Wang <zhwang110@gmail.com>
2024-03-22 11:00:52 -0400

    update localization, generation, and validation script

--- script/fault_localization.py
+++ script/fault_localization.py
@@ -1,3 +1,27 @@
 import pandas as pd
 import os 
+import json
 
+def get_completion(client, prompt):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        # model="gpt-4",
+        messages=messages,
+        response_format={"type": "json_object"},
+        temperature=0.3,
+    )
+    return response.choices[0].message.content
+
+def fault_localization(client, prompt, few_shots, save_file_path ):
+    response = get_completion(client, json.dumps(prompt))
+    with open(save_file_path, 'w') as file:
+       json.dump(json.loads(response), file, indent=4)
+
+
+prompt_localization = {
+  "Role": "As a professional developers. You are responsible for generating program repair patch.",
+  "Instruction": "Read ",
+  "Question": """ Question1: 
+  """
+}           
--- script/patch_generation.py
+++ script/patch_generation.py
@@ -1,3 +1,30 @@
 import pandas as pd
 import os 
+import json
 
+def get_completion(client, prompt):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        # model="gpt-4",
+        messages=messages,
+        response_format={"type": "json_object"},
+        temperature=0.3,
+    )
+    return response.choices[0].message.content
+
+def patch_generation(client, prompt, few_shots, save_file_path ):
+    response = get_completion(client, json.dumps(prompt))
+    with open(save_file_path, 'w') as file:
+       json.dump(json.loads(response), file, indent=4)
+
+def load_data():
+    pass 
+
+
+prompt_geneartion = {
+  "Role": "As a professional developers. You are responsible for generating program repair patch.",
+  "Instruction": "Read ",
+  "Question": """ Question1: 
+              """
+}
