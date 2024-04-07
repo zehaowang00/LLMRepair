27cfcd475a31bfcafe9e32cab7058d3f56ce79fe
Zehao Wang <zhwang110@gmail.com>
2024-04-07 19:27:23 -0400

    add evaluation for file level accuracy

--- llm_repair/fault_local_eval.py
+++ llm_repair/fault_local_eval.py
@@ -7,7 +7,7 @@ def load_json(file_path):
         data = json.load(file)
         return data
 
-def check_method(method_code, ground_method_truth):
+def check_method_level(method_code, ground_method_truth):
     if len(ground_method_truth) == 0:
         return "no ground truth method"
     if len(ground_method_truth) > 0:
@@ -19,8 +19,24 @@ def check_method(method_code, ground_method_truth):
         return "True"
     else:
         return "False"
+    
+def check_file_level(file_name, ground_method_truth, if_has_bug):
+    if len(ground_method_truth) > 0:
+        ground_file_name = ground_method_truth.split('.')[-2]
+        if if_has_bug == "Yes":
+            if ground_file_name in file_name:
+                return "True"
+            else:
+                return "False"
+        else:
+            if ground_file_name in file_name:
+                return "False"
+            else:
+                return "True"
+    else:
+        return "no ground truth method"
 
-def check_block(block_code, bug_id):
+def check_block_level(block_code, bug_id):
     pass
 
 
@@ -43,5 +59,10 @@ for project in projects:
             dicts.append(falut_localization_info)
 
 fault_df = pd.DataFrame(dicts)
-fault_df['Locate Correct Method'] = fault_df.apply(lambda row: check_method(row['method_level'], row['Ground_Method_Truth']), axis=1)
+fault_df['Locate Correct Method'] = fault_df.apply(lambda row: check_method_level(row['method_level'], row['Ground_Method_Truth']), axis=1)
+fault_df['Locate Correct File'] = fault_df.apply(lambda row: check_file_level(row['file_name'], row['Ground_Method_Truth'], row['if_has_bug']), axis=1)
 fault_df.to_csv("../analysis_result/evaluation/fault_location_eval.csv", index=False)
+right_method_df = fault_df[fault_df['Locate Correct Method'] == 'True']
+right_file_df = fault_df[fault_df['Locate Correct File'] == 'True']
+print("File level accuracy:" + str(len(right_file_df)/ len(fault_df)))
+print("Method Level accuracy: " + str(len(right_method_df) / len(fault_df)))
